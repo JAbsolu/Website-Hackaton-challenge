ece3fc441b261217f6c7f5c87592a7bac7826aeb
Johnson Absolu <frefrebiz@hotmail.com>
2023-02-18 16:20:07 -0500

    updates home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
 import Home from './home/home';
 import Signin from './signin/signin';
 import SignUp from './signup/signup';
 import Dashboard from './dashboard/dashboard';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path="/" element={<Home />} />
-    //   <Route path="/dashboard" element={<Dashboard />} />
-    //   <Route path="/signin" element={<Signin />} />
-    //   <Route path="/signup" element={<SignUp />} />
-    // </Routes>  
-    <div>
-      <Home/>
-    </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Home/>}>
+    //       <Route path='/signin'>{<Signin/>}</Route>
+    //       <Route path='/signup'>{<SignUp/>}</Route>
+    //       <Route path='/dashboard'>{<Dashboard/>}</Route>
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <Home/>
   )
 };
 
