fe3a289d81d36453727f2903094221511598a04c
Johnson Absolu <frefrebiz@hotmail.com>
2023-02-18 16:32:22 -0500

    updates routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './home/home';
 import Signin from './signin/signin';
 import SignUp from './signup/signup';
@@ -9,11 +9,9 @@ import Dashboard from './dashboard/dashboard';
 const App = () => {
   return (
     <Routes>
-      <Route path="/'" element={<Home />}></Route>
-      <Route path="'/dashboard/dashboard'" element={<Dashboard />}></Route>
-      <Route path="/signin/signin" element={<Signin />}></Route>
-      <Route path="/signup/signup" element={<SignUp />}></Route>
-    </Routes>  )
+    <Route path='/' element={<Home/>} />
+    </Routes>
+  )
 };
 
 export default App;
