8b48cfc48a6939f2a5627638d0c44f6ad5d23862
Jabsolu <frefrebiz@hotmail.com>
2023-02-27 18:34:50 -0500

    Added community page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './home/home';
 import SigninPage from './signin/signin';
 import SignUp from './signup/signup';
 import Dashboard from './dashboard/dashboard';
+import Community from './community/community';
 import {Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/signin" element={<SigninPage />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/communities" element={<Community/>} />
     </Routes>  
   )
 };
